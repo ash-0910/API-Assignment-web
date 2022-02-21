af2ebb22aa123273fe5de5911eb1c877685644a3
ash-0910 <A00244471@mycambrian.ca>
2022-02-21 18:05:13 -0500

    added tests for subtraction , division and multiplication

--- Math-Operation-Library/MathOpterations.cs
+++ Math-Operation-Library/MathOpterations.cs
@@ -36,5 +36,108 @@ namespace Math_Operation_Library
 
 
         }
+
+        //library for Subtraction 
+        public static int Subtraction(String series)
+        {
+            int answer = 0;
+
+            String[] numbers = series.Split(' ');
+
+            //running foreach loop to iterate through the array of numbers
+            foreach (string n in numbers)
+            {
+
+                int temp;
+
+                //converting the string to int
+                if (int.TryParse(n, out temp))
+                {
+
+                    //subtracting the int to answer
+                    answer -= temp;
+
+                    //checking if the answer is less than 0 in the first run 
+                    if (answer < 0)
+                    {
+                        //if yes then assigning the temp value to answer 
+                        answer = temp;
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+
+            }
+
+            //returning the answer 
+            return answer;
+
+
+        }
+
+
+        //library for Multiplication 
+        public static int Multiplication(String series)
+        {
+            //setting the value of answer as 1 
+            int answer = 1;
+
+            String[] numbers = series.Split(' ');
+
+            //running foreach loop to iterate through the array of numbers
+            foreach (string n in numbers)
+            {
+
+                int temp;
+
+                if (int.TryParse(n, out temp))
+                {
+
+                    //multiplying the numbers and saving in answer 
+                    answer *= temp;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+
+            }
+
+            //returning the answer 
+            return answer;
+
+
+        }
+
+        //library for Division 
+        public static int Division(String series)
+        {
+            int answer;
+
+            String[] numbers = series.Split(' ');
+
+            //taking the splitted numbers in two separate variables
+            int a = Int32.Parse(numbers[0]);
+            int b = Int32.Parse(numbers[1]);
+
+            //checking if any of the numbers are 0 
+            if (a == 0 || b == 0)
+            {
+                //if yes then throwing an exception
+                throw new ArgumentException("Cannot divide by 0 ");
+            }
+            else
+            {
+                //if no then dividing the numbers 
+                answer = a / b;
+            }
+
+            //returning the answer 
+            return answer;
+
+
+        }
     }
 }
--- Math-testing/MathTests.cs
+++ Math-testing/MathTests.cs
@@ -16,7 +16,34 @@ namespace Math_testing
             Assert.AreEqual(expected, MathOpterations.Addition(input));
         }
 
-      
+        //test method for subtraction
+        [TestMethod]
+        [DataRow("5 1", 4)]
+        [DataRow("15 5", 10)]
+        public void StringSub(string input, int expected)
+        {
+            Assert.AreEqual(expected, MathOpterations.Subtraction(input));
+        }
+
+        //test method for multiplication
+        [TestMethod]
+        [DataRow("7 2", 14)]
+        [DataRow("10 5", 50)]
+        public void StringMul(string input, int expected)
+        {
+            Assert.AreEqual(expected, MathOpterations.Multiplication(input));
+        }
+
+        //test method for division
+        [TestMethod]
+        [DataRow("4 2", 2)]
+        [DataRow("18 3", 6)]
+        public void StringDiv(string input, int expected)
+        {
+            Assert.AreEqual(expected, MathOpterations.Division(input));
+        }
+
+
 
     }
 }
