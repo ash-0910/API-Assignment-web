3b3797f4e68eb2f1bebbe7ba65f0904ff0e03f42
ash-0910 <A00244471@mycambrian.ca>
2022-02-21 17:58:18 -0500

    Created Test For Addition

--- Math-Operation-Library/MathOpterations.cs
+++ Math-Operation-Library/MathOpterations.cs
@@ -4,5 +4,37 @@ namespace Math_Operation_Library
 {
     public class MathOpterations
     {
+        //library for addition 
+        public static int Addition(String series)
+        {
+            int answer = 0;
+
+            String[] numbers = series.Split(' ');
+
+            //running foreach loop to iterate through the array of numbers
+            foreach (string n in numbers)
+            {
+
+                int temp;
+
+                //converting the string to int
+                if (int.TryParse(n, out temp))
+                {
+
+                    //adding the int to answer
+                    answer += temp;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+
+            }
+
+            //returning the answer 
+            return answer;
+
+
+        }
     }
 }
--- Math-testing/MathTests.cs
+++ Math-testing/MathTests.cs
@@ -1,5 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using Math_Operation_Library;
 
 
 namespace Math_testing
@@ -7,6 +7,15 @@ namespace Math_testing
     [TestClass]
     public class MathTests
     {
+        //test method for addition
+        [TestMethod]
+        [DataRow("1 1", 2)]
+        [DataRow("5 5", 10)]
+        public void StringAdd(string input, int expected)
+        {
+            Assert.AreEqual(expected, MathOpterations.Addition(input));
+        }
+
       
 
     }
