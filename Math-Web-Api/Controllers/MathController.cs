9f0601f76c93430bb7a94d80e144c6d8faee0166
ash-0910 <A00244471@mycambrian.ca>
2022-02-21 17:51:05 -0500

    Add project files.

---
+++ Math-Web-Api/Controllers/MathController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Math_Web_Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+
+        //the
+        [HttpGet]
+        public double Addition(int lValue , int rValue)
+        {
+            return lValue + rValue;
+        }
+
+        [HttpGet]
+        public double Subtraction(int lValue, int rValue)
+        {
+            return lValue - rValue;
+        }
+
+        [HttpGet]
+        public double Multiplication(int lValue, int rValue)
+        {
+            return lValue * rValue;
+        }
+
+        [HttpGet]
+        public double Division(int lValue, int rValue)
+        {
+            if(lValue==0 || rValue == 0)
+            {
+                throw new ArgumentException("Cannot Divide By 0");
+            }
+            else
+            {
+                return lValue / rValue;
+
+            }
+            
+        }
+
+    }
+}
---
+++ Math-testing/MathTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Math_testing
+{
+    [TestClass]
+    public class MathTests
+    {
+
+    }
+}
